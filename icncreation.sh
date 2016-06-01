01efffccced1c761b8fd7dd208bb59fc89eddd34
Jordi Ortiz <jordi.ortiz@um.es>
2016-06-01 21:32:45 +0200

    Create Proxy in script ok

--- icncreation.sh
+++ icncreation.sh
@@ -1,3 +1,7 @@
+export CONTROLLER='localhost:8181'
+
+curl -u karaf:karaf -X POST --data @data/proxy.json --header 'Content-Type: application/json' http://192.168.122.78:8181/onos/icn/proxy\?name=proxy01
+curl -u karaf:karaf -X GET http://192.168.122.78:8181/onos/icn/proxies
 CONTROLLER='localhost:8181'
 
 curl -u karaf:karaf -X POST --data @data/proxy.json --header 'Content-Type: application/json' http://192.168.122.78:8181/onos/icn/proxy\?name=proxy01
