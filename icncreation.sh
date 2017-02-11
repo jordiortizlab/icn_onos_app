b141e96333f10ab6af4b0e97065de2fecbd14a44
Jordi Ortiz <jordi.ortiz@um.es>
2017-02-11 12:02:46 +0100

    ICN creation configuration for first attempt

--- icncreation.sh
+++ icncreation.sh
@@ -1,21 +1,21 @@
-export CONTROLLER='localhost:8181'
+export CONTROLLER='10.7.0.4:8181'
 
-curl -u karaf:karaf -X POST --data @data/proxy.json --header 'Content-Type: application/json' http://192.168.122.78:8181/onos/icn/proxy\?name=proxy01
-curl -u karaf:karaf -X GET http://192.168.122.78:8181/onos/icn/proxies
+curl -u karaf:karaf -X POST --data @data/proxy.json --header 'Content-Type: application/json' http://$CONTROLLER/onos/icn/proxy\?name=proxy01
+curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/proxies
 echo ""
 
-curl -u karaf:karaf -X POST --data @data/cdn2.json --header 'Content-Type: application/json' http://192.168.122.78:8181/onos/icn/cdn\?name=vplocan
-curl -u karaf:karaf -X GET http://192.168.122.78:8181/onos/icn/cdns
+curl -u karaf:karaf -X POST --data @data/cdn2.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/cdn\?name=vplocan
+curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/cdns
 echo ""
 
-curl -u karaf:karaf -X POST --data @data/cache1.json --header 'Content-Type: application/json' http://192.168.122.78:8181/onos/icn/cache\?name=vplocan\&cname=squid01
+curl -u karaf:karaf -X POST --data @data/cache1.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/cache\?name=vplocan\&cname=squid01
 
-curl -u karaf:karaf -X POST --data @data/cache2.json --header 'Content-Type: application/json' http://192.168.122.78:8181/onos/icn/cache\?name=vplocan\&cname=squid02
-curl -u karaf:karaf -X GET http://192.168.122.78:8181/onos/icn/caches\?name=vplocan
+#curl -u karaf:karaf -X POST --data @data/cache2.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/cache\?name=vplocan\&cname=squid02
+curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/caches\?name=vplocan
 echo ""
 
-curl -u karaf:karaf -X POST --data @data/provider2.json --header 'Content-Type: application/json' http://192.168.122.78:8181/onos/icn/provider\?name=vplocan\&cname=plocan
-curl -u karaf:karaf -X GET http://192.168.122.78:8181/onos/icn/providers\?name=vplocan
+curl -u karaf:karaf -X POST --data @data/providerplexp.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/provider\?name=vplocan\&cname=plocan
+curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/providers\?name=vplocan
 echo ""
 
 
