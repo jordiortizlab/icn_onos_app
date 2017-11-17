1a8b3c4aefffad80e515e4b4d5a8f81bf88d85e9
Jordi Ortiz <jordi.ortiz@um.es>
2017-11-17 07:24:25 -0500

    Update urls so that they work with ubuntu curl

--- icncreationitecuniklu.sh
+++ icncreationitecuniklu.sh
@@ -7,28 +7,28 @@ fi
 echo CONTROLLER: $CONTROLLER
 
 curl -u karaf:karaf -X POST --data @data/proxy.json --header 'Content-Type: application/json' http://$CONTROLLER/onos/icn/proxy\?name=proxyMAT
-curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/proxies
+curl -u karaf:karaf -X GET http://$CONTROLLER/onos/icn/proxies
 echo ""
 
-curl -u karaf:karaf -X POST --data @data/icnuniklu.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/icn\?name=uniklu
-curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/icns
+curl -u karaf:karaf -X POST --data @data/icnuniklu.json --header 'Content-Type: application/json' http://$CONTROLLER/onos/icn/icn\?name=uniklu
+curl -u karaf:karaf -X GET http://$CONTROLLER/onos/icn/icns
 echo ""
 
-curl -u karaf:karaf -X POST --data @data/cache1.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/cache\?name=uniklu\&cname=squid01
+curl -u karaf:karaf -X POST --data @data/cache1.json --header 'Content-Type: application/json' http://$CONTROLLER/onos/icn/cache\?name=uniklu\&cname=squid01
 
-#curl -u karaf:karaf -X POST --data @data/cache2.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/cache\?name=uniklu\&cname=squid02
-curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/caches\?name=uniklu
+#curl -u karaf:karaf -X POST --data @data/cache2.json --header 'Content-Type: application/json' http://$CONTROLLER/onos/icn/cache\?name=uniklu\&cname=squid02
+curl -u karaf:karaf -X GET http://$CONTROLLER/onos/icn/caches\?name=uniklu
 echo ""
 
-curl -u karaf:karaf -X POST --data @data/provideruniklu.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/provider\?name=uniklu\&cname=uniklu
-curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/providers\?name=uniklu
+curl -u karaf:karaf -X POST --data @data/provideruniklu.json --header 'Content-Type: application/json' http://$CONTROLLER/onos/icn/provider\?name=uniklu\&cname=uniklu
+curl -u karaf:karaf -X GET http://$CONTROLLER/onos/icn/providers\?name=uniklu
 
-# curl -u karaf:karaf -X POST --data @data/providerwithinicn.json --header 'Content-Type: application/json' http:///$CONTROLLER/onos/icn/provider\?name=uniklu\&cname=within
-# curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/providers\?name=uniklu
+# curl -u karaf:karaf -X POST --data @data/providerwithinicn.json --header 'Content-Type: application/json' http://$CONTROLLER/onos/icn/provider\?name=uniklu\&cname=within
+# curl -u karaf:karaf -X GET http://$CONTROLLER/onos/icn/providers\?name=uniklu
 
 
 echo ""
-curl -u karaf:karaf -X GET http:///$CONTROLLER/onos/icn/icns
+curl -u karaf:karaf -X GET http://$CONTROLLER/onos/icn/icns
 echo ""
 
 echo "Put devices info"
