9e2fcdf6b48b12e5a0983da40deff2cd8e2e955a
Jordi Ortiz <jordi.ortiz@um.es>
2017-12-13 20:46:55 +0000

    Wait for onos to be ready

--- preparescenario.sh
+++ preparescenario.sh
@@ -12,7 +12,14 @@
 ssh 10.7.0.4 -t sudo docker stop onos_phd
 ssh 10.7.0.4 -t sudo docker rm onos_phd
 ssh 10.7.0.4 -t sudo docker run -d -i -e KARAF_DEBUG=true -e ONOS_APPS=openflow --name onos_phd -p 6633:6633 -p 8181:8181 -p 8101:8101 -p 5005:5005 -p 8080:8080 onosproject/onos:1.10.2
-sleep 60
+
+nc -z 10.7.0.4 8101
+while [ $? -ne 0 ]
+do
+    echo "Wainting for 10.7.0.4 to become available"
+    sleep 60
+    nc -z 10.7.0.4 8101
+done
 # install app
 curl -sS --user karaf:karaf --noproxy localhost -X POST -HContent-Type:application/octet-stream http://10.7.0.4:8181/onos/v1/applications?activate=true --data-binary @icn-sdn-1.3-SNAPSHOT.oar
 sleep 15
