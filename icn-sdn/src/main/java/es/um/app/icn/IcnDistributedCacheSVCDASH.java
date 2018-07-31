164a591b85e66da1992b2d3e9520f74325847a95
Jordi Ortiz <jordi.ortiz@um.es>
2018-07-31 18:53:48 +0200

    IcnDistributedCacheDASH: Use cache precompilation to have faster response

--- icn-sdn/src/main/java/es/um/app/icn/IcnDistributedCacheSVCDASH.java
+++ icn-sdn/src/main/java/es/um/app/icn/IcnDistributedCacheSVCDASH.java
@@ -9,12 +9,20 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IcnDistributedCacheSVCDASH extends IcnClosestCacheDASH {
     private final static Logger log = LoggerFactory.getLogger(IcnDistributedCacheSVCDASH.class);
     static final String DESCRIPTION = "DISTRIBUTEDSVCDASH";
+
+    private ConcurrentHashMap<String, ConcurrentHashMap<String, IMiddlebox>> precomputedCachesXresourceXurl;
+
+    public IcnDistributedCacheSVCDASH() {
+        super();
+        precomputedCachesXresourceXurl = new ConcurrentHashMap<>();
+    }
 
     @Override
     public Cache findCacheForNewResource(IcnService service, String uri, DeviceId sw, PortNumber inPort) {
@@ -35,27 +43,50 @@ public class IcnDistributedCacheSVCDASH extends IcnClosestCacheDASH {
             if (!x.getType().equals(ResourceHTTPDASH.DESCRIPTION))
                 return false;
             ResourceHTTPDASH r = (ResourceHTTPDASH) x;
-            RepresentationDASH representationDASH = r.representation4URL(resourceHTTP);
-            if (representationDASH != null)
-                return true;
-            return false;
+            return r.containsURL(uri);
         }).findFirst();
 
         ResourceHTTPDASH rfull = (ResourceHTTPDASH)resourceOpt.orElse(null);
-        RepresentationDASH representationDASH = rfull.representation4URL(resourceHTTP);
+        if (rfull == null) {
+            log.error("There is no resource available for uri {}", uri);
+            return null;
+        }
+
+        // Look for precomputed cache
+        ConcurrentHashMap<String, IMiddlebox> precomputedCaches = precomputedCachesXresourceXurl.getOrDefault(rfull.getFullurl(), null);
+        if (precomputedCaches != null) {
+            log.info("Locating precomputed cache");
+            IMiddlebox precomputedcache = precomputedCaches.getOrDefault(uri, null);
+            if (precomputedcache != null) {
+                return (Cache)precomputedcache;
+            }
+        }
+
+        // Precompute all caches
+        log.info("Precomputing caches for {}", rfull.getFullurl());
+        ConcurrentHashMap<String, IMiddlebox> cacheXurl = new ConcurrentHashMap<>();
+        ConcurrentHashMap<String, Integer> fullUrlsRepresentationIds = rfull.getFullUrlsRepresentationIds();
+
         Integer representationCount = rfull.getRepresentationCount();
         Double dependencycacheratio = Math.ceil(representationCount / caches.size()); // Next integer
         log.debug("DependenciesCache Ratio: {}", dependencycacheratio);
         List<Integer> representationIds = rfull.getRepresentationIds();
-        int representationIdx = representationIds.indexOf(representationDASH.getId());
-        log.debug("Request representation index {} of {}", representationIdx, representationCount);
-        Double position = Math.floor(representationIdx / dependencycacheratio); // Previous integer
-        log.debug("Position selected {}", position.intValue());
         List<IMiddlebox> orderedcachelist = middleBoxesDistance.entrySet().stream().sorted(new IntegerValueComparator()).map(Map.Entry::getKey).collect(Collectors.toList());
         log.debug("Cache list: {}", orderedcachelist.toString());
-        IMiddlebox iMiddlebox = orderedcachelist.get(position.intValue());
-        log.debug("Selected cache {}", iMiddlebox.getName());
-        return (Cache)iMiddlebox;
+
+        fullUrlsRepresentationIds.entrySet().parallelStream().forEach(x -> {
+            int representationIdx = representationIds.indexOf(x.getValue());
+            log.debug("Request representation index {} of {}", representationIdx, representationCount);
+            Double position = Math.floor(representationIdx / dependencycacheratio); // Previous integer
+            log.debug("Position selected {}", position.intValue());
+            IMiddlebox iMiddlebox = orderedcachelist.get(position.intValue());
+            log.debug("Selected cache {}", iMiddlebox.getName());
+            cacheXurl.put(x.getKey(), iMiddlebox);
+        });
+
+        precomputedCachesXresourceXurl.put(rfull.getFullurl(), cacheXurl);
+
+        return (Cache)cacheXurl.getOrDefault(uri, null);
     }
 
     class IntegerValueComparator implements Comparator<Map.Entry<IMiddlebox, Integer>> {
